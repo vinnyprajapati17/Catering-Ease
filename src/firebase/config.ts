f6076fd048ee3227f865c1aa69f05c8605024fd4
Vinny Prajapati <vinnyprajapati17@gmail.com>
2025-11-30 07:49:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-941304656-cfc70",
+  "appId": "1:8428951522:web:5ccb5d37f57a173bdccbbb",
+  "apiKey": "AIzaSyBsMeDhl_mD8PEy13YnyAi5kC5acLklFDg",
+  "authDomain": "studio-941304656-cfc70.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "8428951522"
+};
